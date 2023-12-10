bd4b5fa5731052c321aba497c57a62d2799c371e
unknown <Narsing@DESKTOP-21T6B4I>
2023-12-10 19:20:04 +0000

    add

--- src/test/java/mavenforjenkins/UITest.java
+++ src/test/java/mavenforjenkins/UITest.java
@@ -15,7 +15,7 @@ public class UITest
 
 	@Parameters("Browser")
 	@Test
-	public void startBrowser(String browserName)
+	public void startBrowser(@Optional("defaultBrowser")String browserName)
 	{
 		System.out.println("Parameter value is "+browserName);
 		WebDriver driver=null;
